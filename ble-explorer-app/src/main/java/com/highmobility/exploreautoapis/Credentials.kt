7989d8e51056479bcda85e79ff4b1faae8200f02
tonis <tonis@Toniss-MacBook-Pro.local>
2020-09-29 16:12:41 +0700

    fix lint warnings

--- ble-explorer-app/src/main/java/com/highmobility/exploreautoapis/Credentials.kt
+++ ble-explorer-app/src/main/java/com/highmobility/exploreautoapis/Credentials.kt
@@ -3,12 +3,12 @@ package com.highmobility.exploreautoapis
 import android.content.Context
 
 class Credentials(context: Context) {
-    val resources = context.resources
-    val packageName = context.packageName
+    private val resources = context.resources
+    private val packageName = context.packageName
 
-    val environment: String? by lazy { getResource("environment") }
+    private val environment: String? by lazy { getResource("environment") }
 
-    fun getResource(key: String): String? {
+    private fun getResource(key: String): String? {
         return try {
             val resId = resources.getIdentifier(key, "string", packageName)
             resources.getString(resId)
